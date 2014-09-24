6019b483f96790f64ef5b080c89c4f9fb68d8126
Paul Zellmer <paulzellmer9@gmail.com>
2014-09-24 10:11:58 -0700

    rand fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 module.exports = function Route(app){
 
   app.get('/', function (req, res){
-    res.render('index', {title:'Rock, Paper Scissors Game'});
+    res.render('index', {title:'Rock, Paper, Scissors Game'});
   });
 
   users = {};
