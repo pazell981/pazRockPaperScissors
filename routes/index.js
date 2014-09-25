977c85ca6e7e423c50a7dd9d3f3094baf43dfda9
Paul Zellmer <paulzellmer9@gmail.com>
2014-09-24 17:35:22 -0700

    player fix

--- routes/index.js
+++ routes/index.js
@@ -32,36 +32,35 @@ module.exports = function Route(app){
     if (req.data.player2==0){
       req.io.join(req.data.player1)
       room[req.data.player1] = {room_id: req.data.player1, player1: users[req.data.player1], player2: users[req.data.player2]}
-      console.log('room', room);
-      app.io.room(req.data.player1).broadcast('room_create', room)
+      app.io.room(req.data.player1).broadcast('room_create', room[req.data.player1])
     } else {
       req.io.join(req.data.player2)
       room[req.data.player2] = {room_id: req.data.player2, player1: users[req.data.player1], player2: users[req.data.player2]}
-      console.log('room', room);
-      app.io.room(req.data.player2).broadcast('room_create', room)
+      app.io.room(req.data.player2).broadcast('room_create', room[req.data.player2])
     }
   })
 
-  app.io.route('denied', function (req){
-    room[req.data.player2] = {room_id: req.data.player2, player1: users[req.data.player1], player2: users[req.data.player2]};
-    delete room[req.data.player2];
-    app.io.room(req.data.player2).broadcast('denied', room);
+  app.io.route('invite_denied', function (req){
+    deny = room[req.data.player2]
+    delete room[req.data.player2]
+    app.io.room(req.data.player2).broadcast('denied', deny);
   })
   
   app.io.route('play', function (req){
-    console.log(room[req.data.room].player2.user_id)
-    if (room[req.data.room].player2.user_id==0){
-      compplay = cplay[(Math.random(2)+1)];
-      console.log(compplay);
-      req.io.room(req.data.room).broadcast('round', compplay)
+    play_room = room[req.data.room];
+    if (play_room.player2.user_id!=0){
+      req.io.room(play_room.room_id).broadcast('round', req.data.play)    
     } else {
-      req.io.room(req.data.room).broadcast('round', req.data.play)
+      key = Math.floor(Math.random()*3);
+      compplay = {play: cplay[key]};
+      app.io.room(play_room.room_id).broadcast('round', {play: cplay[key]})
     }
   })
 
   app.io.route('quit', function (req){
+    quit = room[req.data.player2]
     delete room[req.data.room]
-    req.io.room(req.data.room).broadcast('user_quit')
+    req.io.room(req.data.room).broadcast('user_quit', quit)
   })
  	
  	app.io.route('disconnect', function (req){
